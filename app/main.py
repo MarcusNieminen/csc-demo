543e98922799d90c4ce62fe6cbdc5e93acfc2688
MarcusNieminen <marcus@nieminen.me>
2026-03-30 11:47:52 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -22,4 +22,4 @@ def get_ip(request: Request):
 
 @app.get("/rooms")
 def get_rooms():
-    return ["room1":{"beds":2},"room2":{"beds":3},"room3":{"beds":2}]
+    return {"hotel1":["room1":{"beds":2},"room2":{"beds":3},"room3":{"beds":2}]}
